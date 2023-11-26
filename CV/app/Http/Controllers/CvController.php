19d05be96fc6b5a45a03ae9771ecad41a5667a9d
Mohamed KONATÉ <ing.mohamedkonate@gmail.com>
2023-11-26 16:13:00 +0000

    update

--- CV/app/Http/Controllers/CvController.php
+++ CV/app/Http/Controllers/CvController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cv;
+use App\Models\Employe;
 use Illuminate\Http\Request;
 
 class CvController extends Controller
@@ -28,7 +30,49 @@ class CvController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        // Validation des données reçues
+        $validatedData = $request->validate([
+            'matricule' => 'required|string',
+            'prenom' => 'required|string',
+            'nom' => 'required|string',
+            'sexe' => 'required|in:M,F',
+            'service' => 'required|string',
+            'grade' => 'required|string',
+            'corps' => 'required|string',
+            'fichier' => 'required|file',
+        ]);
+
+//        dd($request->all(),$request->hasFile('fichier'));
+        // Traitement des données (par exemple, enregistrer dans la base de données)
+        // Ici, vous pourriez créer une nouvelle instance de votre modèle et sauvegarder les données
+
+        $employee = new Employe();
+        $employee->matricule = $validatedData['matricule'];
+        $employee->first_name = $validatedData['prenom'];
+        $employee->last_name = $validatedData['nom'];
+        $employee->sexe = $validatedData['sexe'];
+        $employee->service = $validatedData['service'];
+        $employee->grade = $validatedData['grade'];
+        $employee->corps = $validatedData['corps'];
+
+        if ($employee->save()) {
+            if ($request->hasFile('fichier')) {
+                // Stocker le fichier dans le système de fichiers
+                $path = $request->file('fichier')->store('dossier', 'public');
+                $cv = new Cv();
+                $cv->id_employee = $employee->id;
+                $cv->path = $path;
+                $cv->save();
+
+                // Vous pouvez enregistrer $path dans votre base de données si nécessaire
+            }
+        } else {
+        }
+
+
+        // Redirection ou autre logique post-enregistrement
+        return redirect()->route('cv-import');
     }
 
     /**
--- CV/resources/views/pages/CV/importe.blade.php
+++ CV/resources/views/pages/CV/importe.blade.php
@@ -4,84 +4,87 @@
         <div class="page-header">
             <div class="page-title">
                 <h4>Import - CV</h4>
-{{--                <h6>Create new product Category</h6>--}}
+                {{--                <h6>Create new product Category</h6>--}}
             </div>
         </div>
 
         <div class="card">
             <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-3 col-sm-6 col-12">
-                        <div class="form-group">
-                            <label>Matricule</label>
-                            <input type="text">
+                <form action="{{route('cv-import-store')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Matricule</label>
+                                <input type="text" name="matricule">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 col-12">
-                        <div class="form-group">
-                            <label>Prénom</label>
-                            <input type="text">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Prénom</label>
+                                <input type="text" name="prenom">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 col-12">
-                        <div class="form-group">
-                            <label>Nom</label>
-                            <input type="text">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Nom</label>
+                                <input type="text" name="nom">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 col-12">
-                    <div class="form-group">
-                        <label>Sexe</label>
-                        <select class="form-control" name="sexe">
-                            <option value="M">M</option>
-                            <option value="F">F</option>
-                        </select>
-                    </div>
-                    </div>
-                  <div class="col-lg-3 col-sm-6 col-12">
-                        <div class="form-group">
-                        <label>Services</label>
-                        <select class="form-control" name="sexe">
-                            <option value="cifp">CIFP</option>
-                            <option value="acct">ACCT</option>
-                            <option value="acct">PGT</option>
-                            <option value="acct">RGD</option>
-                            <option value="acct">TR</option>
-                            <option value="acct">RP</option>
-                            <option value="acct">DNTCP</option>
-                        </select>
-                    </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 col-12">
-                        <div class="form-group">
-                            <label>Grade</label>
-                            <input type="text">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Sexe</label>
+                                <select class="form-control" name="sexe">
+                                    <option value="M">M</option>
+                                    <option value="F">F</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 col-12">
-                        <div class="form-group">
-                            <label>Corps</label>
-                            <input type="text">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Services</label>
+                                <select class="form-control" name="service">
+                                    <option value="cifp">CIFP</option>
+                                    <option value="acct">ACCT</option>
+                                    <option value="acct">PGT</option>
+                                    <option value="acct">RGD</option>
+                                    <option value="acct">TR</option>
+                                    <option value="acct">RP</option>
+                                    <option value="acct">DNTCP</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    
-                    <div class="col-lg-12">
-                        <div class="form-group">
-                            <label> Fichier </label>
-                            <div class="image-upload">
-                                <input type="file">
-                                <div class="image-uploads">
-                                    <img src="assets/img/icons/upload.svg" alt="img">
-                                    <h4>Drag and drop a file to upload</h4>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Grade</label>
+                                <input type="text" name="grade">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Corps</label>
+                                <input type="text" name="corps">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label> Fichier </label>
+                                <div class="image-upload">
+                                    <input type="file" name="fichier">
+                                    <div class="image-uploads">
+                                        <img src="assets/img/icons/upload.svg" alt="img">
+                                        <h4>Drag and drop a file to upload</h4>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-lg-12">
+                            <button type="submit" class="btn btn-submit me-2">Charger</button>
+                            <a href="categorylist.html" class="btn btn-cancel">Quitter</a>
+                        </div>
                     </div>
-                    <div class="col-lg-12">
-                        <a href="javascript:void(0);" class="btn btn-submit me-2">Charger</a>
-                        <a href="categorylist.html" class="btn btn-cancel">Quitter</a>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
 
